c82f5705a7c600a10becfecc7976965d9e1f4e29
Mihail Dunaev <mihail.dunaev91@gmail.com>
2017-11-20 19:09:28 +0200

    Fix problem with no new friend in chat list after accepting request

--- src/index.js
+++ src/index.js
@@ -4,7 +4,16 @@ import ReactDOM from "react-dom";
 import MainViewContainer from "./containers/MainViewContainer";
 import {Actions} from "./data/PetbookActions";
 
-$.get("/api/auth", function(auth, status){
+/*
+ * this is needed only when you accept a friend request
+ * /api/auth won't read the auth from db again
+ * you need the new friends list for chat side
+ *
+ * TODO use hooks
+ * also, reading auth again is not that bad, the other
+ * requests take way more time
+ * */
+$.get("/api/update-auth", function(auth, status){
     Actions.update_auth(auth);
 });
 
