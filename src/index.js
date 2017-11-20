255b2b2106ffc7dd831bddfb94c09c1c82ce9b85
Mihail Dunaev <mihail.dunaev91@gmail.com>
2017-11-20 19:30:03 +0200

    Small change in order of get requests

--- src/index.js
+++ src/index.js
@@ -15,18 +15,23 @@ import {Actions} from "./data/PetbookActions";
  * */
 $.get("/api/update-auth", function(auth, status){
     Actions.update_auth(auth);
-});
-
-$.get("/api/friends", function(auth, status){
-    Actions.update_auth(auth);
-});
-
-$.get("/api/friend-requests", function(requests, status){
-    Actions.update_friend_requests(requests);
-});
-
-$.get("/api/posts", function(posts, status){
-    Actions.set_posts(posts);
+    /*
+     * TODO maybe find a better solution to this?
+     * we need req.session.auth to be updated before
+     * the other operations here
+     *
+     * also, both /api/update-auth and /api/friends
+     * will try to set req.session.auth - check locking?
+     * */
+    $.get("/api/friends", function(auth, status){
+	Actions.update_auth(auth);
+    });
+    $.get("/api/friend-requests", function(requests, status){
+	Actions.update_friend_requests(requests);
+    });
+    $.get("/api/posts", function(posts, status){
+	Actions.set_posts(posts);
+    });
 });
 
 ReactDOM.render(
